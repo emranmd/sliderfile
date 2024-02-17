814addd3f62e3bda78b224bb36bfbd44b0d30d4d
emranmd <emran01851384660@gmail.com>
2024-02-17 12:12:31 +0600

    m

---
+++ assets/js/my.js
@@ -0,0 +1,14 @@
+// $('.firstSlider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     variableWidth: true
+//   });
+  $('.sliderContainer').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+  });
